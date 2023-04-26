5b62342c360725a8b96247d91ea58aa6be4cbe9e
pgmccomm <pgmccomm@umich.edu>
2023-04-26 16:48:52 +0000

    Replace waveformGenerator.ino

--- supplementary/Waveforms/waveformGenerator.ino
+++ supplementary/Waveforms/waveformGenerator.ino
@@ -1,11 +1,16 @@
-#include <SPI.h>
 #include "waveforms.h"
+
+//This is the code that ran on the ESP32 in order to generate different signals 
+//using the 8 bit on board DAC. All of the waveforms in waveform.h were generated in 
+//matlab
 
 #define DAC_CH 25
 #define BUTTON 14
 
 int count = 0;
 int i = 0;
+
+
 
 void setup()
 {
@@ -19,7 +24,7 @@ void loop()
     if (digitalRead(BUTTON) == LOW) {  // check again to ensure it is a valid press
       count++;  // increment the count
       delay(300);
-      if (count > 7) {  // wrap around the count if it exceeds the maximum value
+      if (count > 5) {  // wrap around the count if it exceeds the maximum value
         count = 0;
       }
     }
@@ -27,32 +32,23 @@ void loop()
 
   switch (count) {
     case 0:
-      dacWrite(DAC_CH, noisy_mod[i]);
-      Serial.println("clean_sine");
+      dacWrite(DAC_CH, noisy_sine[i]);
       break;
     case 1:  
-      dacWrite(DAC_CH, noisy_sine[i]);
-       Serial.println("noisy_sine");
+      dacWrite(DAC_CH, noisy_saw[i]);
       break;
     case 2:  
-      dacWrite(DAC_CH, clean_saw[i]);
-       Serial.println("clean_saw");
+      dacWrite(DAC_CH, noisy_mod[i]);
       break;
     case 3:  
-       dacWrite(DAC_CH, noisy_saw[i]);
-       Serial.println("noisy_saw");
+       dacWrite(DAC_CH, burst_noise_sine[i]);
       break;
     case 4:  
-      dacWrite(DAC_CH, mod_sine[i]);
-      Serial.println("mod_sine");
-      break;
+     dacWrite(DAC_CH, mod_ramp[i]);
+    break;
     case 5:  
-      dacWrite(DAC_CH, burst_noise_sine[i]);
-      Serial.println("burst_noise_sine");
-      break;
-    case 6:
-      dacWrite(DAC_CH, 0);
-      break;
+     dacWrite(DAC_CH,  mod_sine_100[i]);
+    break;
     default:
       dacWrite(DAC_CH, 0);
       break;
@@ -65,7 +61,9 @@ void loop()
   else{
     i++;
   }
+
   
   //here we can set the "frequency" of the signal sent on the DAC
+  //currently the generated wavforms are 1Hz with 1Khz per discrete ouput value
   delay(1);  // insert delay after each sample output
 }
